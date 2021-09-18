6ef1b27f040c9603d9c54036f4a0981ee0c309a8
EC2 Default User <ec2-user@ip-172-31-23-4.eu-central-1.compute.internal>
2021-09-18 12:03:42 +0000

    Added user password handling.

--- models/user.js
+++ models/user.js
@@ -21,7 +21,30 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-const User = mongoose.model("User", userSchema);
+userSchema.pre('save', async function(next){
+    try{
+        if(!this.isModified("password")){
+            return next();
+        }
+        
+        let hashedPassword = await bcrypt.hash(this.password, 10);
+        this.password = hashedPassword;
+        
+        return next();
+    } catch (err){
+        return next(err);
+    }
+});
 
+userSchema.method.comparePassword = async function(cadidatePassword, next){
+    try{
+        let isMatch = await bcrypt.compapre(cadidatePassword, this.password);
+        return isMatch;
+    } catch(err) {
+        return next(err);
+    }
+}
+
+const User = mongoose.model("User", userSchema);
 module.exports = User;
 
