1dead5f71e327750809f007ceff86fd8c2937343
EC2 Default User <ec2-user@ip-172-31-23-4.eu-central-1.compute.internal>
2021-09-21 08:57:31 +0000

    Added route for messages apis.

--- index.js
+++ index.js
@@ -17,6 +17,21 @@ app.use(bodyParser.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users/:id/messages", loginRequired, ensureCorrectUser, messagesRoutes);
 
+app.get("/api/messages", loginRequired, async function(req, res, next){
+    try{
+        let messages = await db.Message.find()
+            .sort({ createdAt: "desc" })
+            .populate("user", {
+                username: true,
+                profileImageUrl: true
+            });
+            
+            return res.status(200).json(messages);
+    } catch(e) {
+        return next(e);
+    }
+})
+
 app.use( function(req, res, next){
     let err = new Error("Not Found");
     err.state = 404;
